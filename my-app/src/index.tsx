03a6388da7d06a2b2518952e243b962b41b1890c
Jens Jäger <jens.jaeger@mobilex.eu>
2023-11-07 10:28:43 +0100

    log errors

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -6,4 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.addEventListener('error', error => console.error(error));
+
 ReactDOM.render(<App />, document.getElementById('container'));
